e181e7f5ee3a09b7807641a865f02d341c153ff7
Ricardo <ricardo.rodiani@gmail.com>
2020-11-12 20:00:05 -0300

    Adicionadas funcoes que verificam qtd total de acordos e qtd de acordos feito por CPF e CNPJ

--- script.py
+++ script.py
@@ -6,9 +6,9 @@ from datetime import datetime
 # Funcao que le o csv e converte em funcao lista de dicionarios, onde cada dicionario representa um acordo
 with open('teste.csv') as csvfile:
     reader = csv.DictReader(csvfile, delimiter="|")
-    for row in  reader:
+    for row in reader:
         listaDicionarios = list(reader)
-        print(listaDicionarios[0])
+
 
 def diaUtil(stringData):
     date = datetime.strptime(stringData, '%Y-%m-%d').date()
@@ -17,17 +17,28 @@ def diaUtil(stringData):
     else:
         return True
 
+
 def acordoValido(acordo):
     valorContrato = float(acordo['ValorContrato'])
-    valorParcela =  float(acordo['ValorParcela'])
+    valorParcela = float(acordo['ValorParcela'])
     numParcelas = float(acordo['ContratoPlano'])
     data = acordo['DataVencimento']
-    if(valorContrato == valorParcela * numParcelas and diaUtil(data)): 
+    if(valorContrato == valorParcela * numParcelas and diaUtil(data)):
         return True
     else:
         return False
 
-if(acordoValido(listaDicionarios[0])):
-    print('Acordo Valido!')
-else:
-    print('Acordo inválido')
+def qtdAcordos(listaDicionarios):
+    return len(listaDicionarios)
+
+def ehCPF(acordo):
+    return len(acordo['documento']) == 11
+
+def ehCNPJ(acordo):
+    return len(acordo['documento']) == 14
+
+listaCPF = [x for x in listaDicionarios if ehCPF(x)]
+print(len(listaCPF))
+listaCNPJ = [x for x in listaDicionarios if ehCNPJ(x)]
+print(len(listaCNPJ))
+print(qtdAcordos(listaDicionarios))
