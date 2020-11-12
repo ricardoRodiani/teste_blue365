7d94cae35158c64297b632d10ec09aaf99c9694e
Ricardo <ricardo.rodiani@gmail.com>
2020-11-12 18:55:35 -0300

    Adicionada funcao que verifica se acordo eh valido

--- script.py
+++ script.py
@@ -1,7 +1,33 @@
-# Leitura e conversão do arquivo CSV para um dicionario iterativo, usando a funcionalidade DictReader
+# Vamos utilizar as bibliotecas csv para lidar com o arquivo csv e fazer o parser apropiado
 import csv
+# Import datetime para lidar com as datas
+from datetime import datetime
+
+# Funcao que le o csv e converte em funcao lista de dicionarios, onde cada dicionario representa um acordo
 with open('teste.csv') as csvfile:
     reader = csv.DictReader(csvfile, delimiter="|")
     for row in  reader:
         listaDicionarios = list(reader)
         print(listaDicionarios[0])
+
+def diaUtil(stringData):
+    date = datetime.strptime(stringData, '%Y-%m-%d').date()
+    if(date.weekday() == 6 or date.weekday() == 7):
+        return False
+    else:
+        return True
+
+def acordoValido(acordo):
+    valorContrato = float(acordo['ValorContrato'])
+    valorParcela =  float(acordo['ValorParcela'])
+    numParcelas = float(acordo['ContratoPlano'])
+    data = acordo['DataVencimento']
+    if(valorContrato == valorParcela * numParcelas and diaUtil(data)): 
+        return True
+    else:
+        return False
+
+if(acordoValido(listaDicionarios[0])):
+    print('Acordo Valido!')
+else:
+    print('Acordo inválido')
