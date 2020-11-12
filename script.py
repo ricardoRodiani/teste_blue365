0ba9da8cf273bec5b6aae6092aa3fe4fff31da11
Ricardo <ricardo.rodiani@gmail.com>
2020-11-12 20:37:06 -0300

    Funcao somatoria do valor total em reais de todos os contratos

--- script.py
+++ script.py
@@ -2,6 +2,7 @@
 import csv
 # Import datetime para lidar com as datas
 from datetime import datetime
+import locale
 
 # Funcao que le o csv e converte em funcao lista de dicionarios, onde cada dicionario representa um acordo
 with open('teste.csv') as csvfile:
@@ -28,25 +29,50 @@ def acordoValido(acordo):
     else:
         return False
 
+
 def qtdAcordos(listaDicionarios):
     return len(listaDicionarios)
+
 
 def ehCPF(acordo):
     return len(acordo['documento']) == 11
 
+
 def ehCNPJ(acordo):
     return len(acordo['documento']) == 14
 
-listaCPF = [x for x in listaDicionarios if ehCPF(x)]
-print(len(listaCPF))
 
-listaCNPJ = [x for x in listaDicionarios if ehCNPJ(x)]
-print(len(listaCNPJ))
+def qtdCPF(listaDicionarios):
+    listaCPF = [x for x in listaDicionarios if ehCPF(x)]
+    return len(listaCPF)
 
-print(qtdAcordos(listaDicionarios))
 
-qtdAcordosInvalidos = 0
-for acordo in listaDicionarios:
-    if(not(acordoValido(acordo))):
-        qtdAcordosInvalidos += 1
-print(qtdAcordosInvalidos)
+def qtdCNPJ(listaDicionarios):
+    listaCNPJ = [x for x in listaDicionarios if ehCNPJ(x)]
+    return len(listaCNPJ)
+
+
+def qtdAcordosInvalidos(listaDicionarios):
+    qtdAcordosInvalidos = 0
+    for acordo in listaDicionarios:
+        if(not(acordoValido(acordo))):
+            qtdAcordosInvalidos += 1
+    
+    return qtdAcordosInvalidos
+
+def qtdTotalAcordos(listaDicionarios):
+    qtdReais = 0
+    for acordo in listaDicionarios:
+        qtdReais += float(acordo['ValorContrato'])
+    
+    locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+    qtdReais = locale.currency(qtdReais, grouping=True, symbol=None)
+    return qtdReais
+
+
+print("Foram gerados {qtd} acordos:".format(qtd = qtdAcordos(listaDicionarios)))
+print("{qtd} de CPF's".format(qtd = qtdCPF(listaDicionarios)))
+print("{qtd} de CNPJ's".format(qtd = qtdCNPJ(listaDicionarios)))
+print("{qtd} acordos são inválidos!".format(qtd = qtdAcordosInvalidos(listaDicionarios)))
+qtdTotalAcordos(listaDicionarios)
+print("Valor total de todos os acordos gerados: R$ {valor}".format(valor = qtdTotalAcordos(listaDicionarios)))
